cdbe13b2992fbf13f43db7648c1b331ee086a0e4
Khoa Pham <khoa.zany@gmail.com>
2014-03-12 05:21:13 +0800

    UI Skeleton Initial Version

---
+++ app/scripts/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('gstApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/user/jenny', {
+        templateUrl: 'views/targetProfile.html',
+        controller: 'MainCtrl'
+      })
+      .when('/result', {
+        templateUrl: 'views/result.html',
+        controller: 'ResultCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+/* 
+  .run(['$rootScope', '$window', 'srvAuth', 
+	function($rootScope, $window, sAuth) 
+    
+    $window.fbAsyncInit = function () {
+        FB.init({
+            appId:'631013896952905',
+            channelUrl: 'views/channel.html',
+            status:true,
+            cookie:true,
+            xfbml:true
+        });
+        
+        FB.Event.subscribe('auth.statusChange', function(response) {
+            $rootScope.$broadcast("fb_statusChange", {'status': response.status});
+        });
+    };
+
+    (function (d) {
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement('script');
+        js.id = id;
+        js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+}]);
+
+*/
